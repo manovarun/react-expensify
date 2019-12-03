6c0f5588317ef69b8407ccdd359086e60406d2b6
manovarun <samvarun@gmail.comn>
2019-12-04 00:29:53 +0530

    Setting React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const ExpenseDashboardPage = () => (
+  <div>This is from expense dashboard component</div>
+);
+
+const AddExpensePage = () => <div>This is from add expense page component</div>;
+
+const EditExpensePage = () => (
+  <div>This is from edit expense page component</div>
+);
+
+const HelpPage = () => <div>This is from help page component</div>;
 
 const App = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste itaque
-      accusantium est consequuntur repudiandae in eveniet dolores aut corrupti
-      cupiditate sit, pariatur rerum soluta deleniti provident ipsam officiis.
-      Excepturi, rerum?
-    </div>
+    <Router>
+      <div>
+        <Route path="/" component={ExpenseDashboardPage} exact />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+      </div>
+    </Router>
   );
 };
 
