1fababbb202c57821c83861827c57b96417828cc
manovarun <samvarun@gmail.comn>
2019-12-07 14:22:54 +0530

    Wiring up Edit Expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -5,14 +5,17 @@ import { SingleDatePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
 
 class ExpenseForm extends Component {
-  state = {
-    description: "",
-    amount: "",
-    note: "",
-    createdAt: moment(),
-    calendarFocused: false,
-    error: null
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: props.expense ? props.expense.description : "",
+      note: props.expense ? props.expense.note : "",
+      amount: props.expense ? (props.expense.amount / 100).toString() : "",
+      createdAt: moment(),
+      calendarFocused: false,
+      error: null
+    };
+  }
 
   onDescriptionChange = e => {
     const description = e.target.value;
